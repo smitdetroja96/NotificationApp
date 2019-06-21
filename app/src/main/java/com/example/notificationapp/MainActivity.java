00228befb9381f512c8c98947f4eae0484e6c698
smitdetroja96 <smitdetroja96.sd@gmail.com>
2019-06-21 19:48:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/notificationapp/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.notificationapp;
+
+import android.app.AlarmManager;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.PendingIntent;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.app.job.JobService;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
+
+import static java.lang.Thread.sleep;
+
+public class MainActivity extends AppCompatActivity {
+
+    AlarmManager alarmManager;
+    TimePicker timePicker;
+    PendingIntent pendingIntent;
+    Calendar calendar;
+    Intent intent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        timePicker = findViewById(R.id.time_picker);
+
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        calendar = Calendar.getInstance();
+        intent = new Intent(MainActivity.this,AlarmReceiver.class);
+
+
+
+    }
+
+    public void Hii(View view) {
+
+            calendar.set(Calendar.HOUR_OF_DAY,timePicker.getHour());
+            calendar.set(Calendar.MINUTE,timePicker.getMinute());
+
+
+            pendingIntent = PendingIntent.getBroadcast(MainActivity.this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis(),pendingIntent);
+
+
+    }
+}
